909af7c122ae5873b54187a079fbf6f3052e28ab
William Kraus <88288206+KrLiam@users.noreply.github.com>
2023-06-26 14:29:43 -0300

    adiciona benchmark --merge

--- Trabalho-2/benchmark.py
+++ Trabalho-2/benchmark.py
@@ -14,12 +14,15 @@ def f(arquivo: str, processos: int, threads: int):
     return g
 
 
+BENCHMARK_JSON = Path.cwd() / "benchmark.json"
+
 def benchmark(
     arquivo: str,
     intervalo_max: int,
     iteracoes: int,
     num_fixo_processos: int = 4,
     num_fixo_threads: int = 1,
+    merge: bool = False,
 ):
     solucoes = ler_solucoes(arquivo)
     intervalo = list(range(1, intervalo_max + 1))
@@ -30,6 +33,20 @@ def benchmark(
     por_threads = [
         timeit(f(arquivo, num_fixo_processos, n), number=iteracoes) for n in intervalo
     ]
+
+    if merge:
+        try:
+            txt = BENCHMARK_JSON.read_text()
+        except FileNotFoundError:
+            txt = "{}"
+        
+        data = json.loads(txt)
+        iteracoes += data.get("iterations", 0)
+        benchmarks = data.get("benchmarks", [{}, {}])
+        prev_por_processos = benchmarks[0].get("tempo", [0] * len(por_processos))
+        prev_por_threads = benchmarks[1].get("tempo", [0] * len(por_threads))
+        por_processos = [a + b for a, b in zip(por_processos, prev_por_processos)]
+        por_threads = [a + b for a, b in zip(por_threads, prev_por_threads)]
 
     speedup_por_processos = [por_processos[0] / n for n in por_processos]
     speedup_por_threads = [por_threads[0] / n for n in por_threads]
@@ -52,7 +69,7 @@ def benchmark(
             },
         ],
     }
-    (Path.cwd() / "benchmark.json").write_text(json.dumps(data, indent=4))
+    BENCHMARK_JSON.write_text(json.dumps(data, indent=4))
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.set_size_inches(12, 5)
@@ -85,13 +102,15 @@ def main():
     parser.add_argument("arquivo_solucoes")
     parser.add_argument("intervalo_max", type=int, default=10)
     parser.add_argument("iteracoes", type=int, default=1)
+    parser.add_argument("--merge", action="store_true", default=False, help="Acumula resultados obtidos com os dados já presentes no arquivo benchmark.json")
 
     args = parser.parse_args()
 
     benchmark(
         args.arquivo_solucoes,
         args.intervalo_max,
-        args.iteracoes
+        args.iteracoes,
+        merge=args.merge,
     )
 
 
