cd4f38f029f4e4bd5a9b74c066e936b6ca7e4b93
William Kraus <88288206+KrLiam@users.noreply.github.com>
2023-06-25 22:39:06 -0300

    calcula speedup e formata grafico

--- Trabalho-2/benchmark.py
+++ Trabalho-2/benchmark.py
@@ -11,13 +11,39 @@ def f(n):
     return g
 
 def benchmark():
+    RANGE = list(range(1, 11))
+    NUM_THREADS = 1
+    NUM_PROCESSOS = 4
+    NUMBER = 5
+
     por_processos = [
-        timeit(f(n), number=5)
-        for n in range(1, 11)
+        timeit(lambda: sudoku("input-sample.txt", n, NUM_THREADS), number=NUMBER)
+        for n in RANGE
+    ]
+    por_threads = [
+        timeit(lambda: sudoku("input-sample.txt", NUM_PROCESSOS, n), number=NUMBER)
+        for n in RANGE
     ]
 
-    plt.plot(range(1, 11), por_processos, "g")
+    speedup_por_processos = [por_processos[0] / n for n in por_processos]
+    speedup_por_threads = [por_processos[0] / n for n in por_threads]
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    ax1.set_title(f"{RANGE[0]}-{RANGE[-1]} Processos, {NUM_THREADS} thread")
+    ax1.plot(RANGE, speedup_por_processos, "r")
+    ax1.set_xbound(RANGE[0], RANGE[-1])
+    ax1.set_xlabel("Número de Processos")
+    ax1.set_ylabel("Speedup")
+
+    ax2.set_title(f"{NUM_PROCESSOS} Processos, {RANGE[0]}-{RANGE[-1]} threads")
+    ax2.plot(RANGE, speedup_por_threads, "r")
+    ax2.set_xbound(RANGE[0], RANGE[-1])
+    ax2.set_xlabel("Número de Threads")
+    ax2.set_ylabel("Speedup")
+
     plt.show()
+
 
 
 if __name__ == "__main__":
